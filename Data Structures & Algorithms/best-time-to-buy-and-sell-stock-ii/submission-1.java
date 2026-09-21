dd56fcdb4add0055b978222df37b61d1a8dd0395
Dilip Kumar <dilipdev7275@gmail.com>
2026-09-21 17:58:35 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        Stack<Integer> st=new Stack<>();
+        int buy=prices[0];
+        int ans=0;
+        for(int i=1;i<prices.length;i++){
+            if(buy<prices[i]){
+                ans+=(prices[i]-buy);
+                buy=prices[i];
+            }else{
+                buy=prices[i];
+            }
+        }
+        return ans;
+    }
+}
